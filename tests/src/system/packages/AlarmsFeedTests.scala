2ea9a22827d3a04dc78ea15a61d43cd4b04c7034
Jason Peterson <jasonpet@us.ibm.com>
2016-12-05 20:51:49 -0500

    fix formatting in tests

--- tests/src/system/packages/AlarmsFeedTests.scala
+++ tests/src/system/packages/AlarmsFeedTests.scala
@@ -14,17 +14,19 @@
  * limitations under the License.
  */
 package system.packages
+
 import org.junit.runner.RunWith
 import org.scalatest.FlatSpec
 import org.scalatest.junit.JUnitRunner
+
 import common.TestHelpers
-//import common.TestUtils.ANY_ERROR_EXIT
 import common.Wsk
 import common.WskProps
 import common.WskTestHelpers
 import spray.json.DefaultJsonProtocol.IntJsonFormat
 import spray.json.DefaultJsonProtocol.StringJsonFormat
 import spray.json.pimpAny
+
 /**
  * Tests for alarms trigger service
  */
@@ -36,20 +38,25 @@ class AlarmsFeedTests
 
     val wskprops = WskProps()
     val wsk = new Wsk
+
     behavior of "Alarms trigger service"
+
     it should "bind alarm package and fire periodic trigger using cron feed" in withAssetCleaner(wskprops) {
         (wp, assetHelper) =>
         implicit val wskprops = wp // shadow global props and make implicit
         val triggerName = s"dummyAlarmsTrigger-${System.currentTimeMillis}"
         val packageName = "dummyAlarmsPackage"
+
         // the package alarms should be there
         val packageGetResult = wsk.pkg.get("/whisk.system/alarms")
         println("fetched package alarms")
         packageGetResult.stdout should include("ok")
+
         // create package binding
         assetHelper.withCleaner(wsk.pkg, packageName) {
             (pkg, name) => pkg.bind("/whisk.system/alarms", name)
         }
+
         // create whisk stuff
         val feedCreationResult = assetHelper.withCleaner(wsk.trigger, triggerName) {
             (trigger, name) =>
@@ -58,18 +65,22 @@ class AlarmsFeedTests
                     "cron" -> "* * * * * *".toJson))
         }
         feedCreationResult.stdout should include("ok")
+
         println("waiting for triggers")
         val activations = wsk.activation.pollFor(N = 5, Some(triggerName), retries = 20).length
         println(s"Found activation size (should be at least 5): $activations")
         activations should be >= 5
+
         // delete the whisk trigger, which must also delete the feed
         wsk.trigger.delete(triggerName)
         Thread.sleep(5000)
         // get activation list after delete of the trigger
         val activationsAfterDelete = wsk.activation.ids(wsk.activation.list(filter = Some(triggerName))).length
         println(s"Found activation size after delete: $activationsAfterDelete")
+
         // recreate the trigger now without the feed
         wsk.trigger.create(triggerName)
+
         // get activation list again, should be same as before sleeping
         println("confirming no new triggers")
         val activationsAfterWait = wsk.activation.pollFor(N = activationsAfterDelete + 1, Some(triggerName)).length
@@ -77,14 +88,17 @@ class AlarmsFeedTests
         println("Activation list after wait should equal with activation list after delete")
         activationsAfterWait should be(activationsAfterDelete)
     }
+
     it should "fire periodic trigger using cron feed using _ namespace" in withAssetCleaner(WskProps()) {
         (wp, assetHelper) =>
         implicit val wskprops = wp // shadow global props and make implicit
         val triggerName = s"dummyAlarmsTrigger-${System.currentTimeMillis}"
+
         // the package alarms should be there
         val packageGetResult = wsk.pkg.get("/whisk.system/alarms")
         println("fetched package alarms")
         packageGetResult.stdout should include("ok")
+
         // create whisk stuff
         val feedCreationResult = assetHelper.withCleaner(wsk.trigger, triggerName) {
             (trigger, name) =>
@@ -94,24 +108,29 @@ class AlarmsFeedTests
                     "maxTriggers" -> 3.toJson))
         }
         feedCreationResult.stdout should include("ok")
+
         // get activation list of the trigger
         val activations = wsk.activation.pollFor(N = 4, Some(triggerName), retries = 15).length
         println(s"Found activation size: $activations")
         activations should be(3)
     }
+
     it should "should only invoke as many times as specified" in withAssetCleaner(wskprops) {
         (wp, assetHelper) =>
         implicit val wskprops = wp // shadow global props and make implicit
         val triggerName = s"dummyAlarmsTrigger-${System.currentTimeMillis}"
         val packageName = "dummyAlarmsPackage"
+
         // the package alarms should be there
         val packageGetResult = wsk.pkg.get("/whisk.system/alarms")
         println("fetched package alarms")
         packageGetResult.stdout should include("ok")
+
         // create package binding
         assetHelper.withCleaner(wsk.pkg, packageName) {
             (pkg, name) => pkg.bind("/whisk.system/alarms", name)
         }
+
         // create whisk stuff
         val feedCreationResult = assetHelper.withCleaner(wsk.trigger, triggerName) {
             (trigger, name) =>
@@ -121,24 +140,29 @@ class AlarmsFeedTests
                     "maxTriggers" -> 3.toJson))
         }
         feedCreationResult.stdout should include("ok")
+
         // get activation list of the trigger
         val activations = wsk.activation.pollFor(N = 4, Some(triggerName)).length
         println(s"Found activation size: $activations")
         activations should be(3)
     }
+
     it should "should not fail when specifying triggers above 1 Million" in withAssetCleaner(wskprops) {
         (wp, assetHelper) =>
         implicit val wskprops = wp // shadow global props and make implicit
         val triggerName = s"dummyAlarmsTrigger-${System.currentTimeMillis}"
         val packageName = "dummyAlarmsPackage"
+
         // the package alarms should be there
         val packageGetResult = wsk.pkg.get("/whisk.system/alarms")
         println("fetched package alarms")
         packageGetResult.stdout should include("ok")
+
         // create package binding
         assetHelper.withCleaner(wsk.pkg, packageName) {
             (pkg, name) => pkg.bind("/whisk.system/alarms", name)
         }
+
         // create whisk stuff
         val feedCreationResult = assetHelper.withCleaner(wsk.trigger, triggerName) {
             (trigger, name) =>
@@ -149,19 +173,23 @@ class AlarmsFeedTests
         }
         feedCreationResult.stdout should include("ok")
     }
+
     it should "should not deny trigger creation when choosing maxTriggers set to infinity (-1)" in withAssetCleaner(wskprops) {
         (wp, assetHelper) =>
         implicit val wskprops = wp // shadow global props and make implicit
         val triggerName = s"dummyAlarmsTrigger-${System.currentTimeMillis}"
         val packageName = "dummyAlarmsPackage"
+
         // the package alarms should be there
         val packageGetResult = wsk.pkg.get("/whisk.system/alarms")
         println("fetched package alarms")
         packageGetResult.stdout should include("ok")
+
         // create package binding
         assetHelper.withCleaner(wsk.pkg, packageName) {
             (pkg, name) => pkg.bind("/whisk.system/alarms", name)
         }
+
         // create whisk stuff
         val feedCreationResult = assetHelper.withCleaner(wsk.trigger, triggerName, confirmDelete = true) {
             (trigger, name) =>
