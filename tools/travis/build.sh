5d0446ea82b5d06c055c2ad9183c8ebc462d5a71
Jason Peterson <jasonpet@us.ibm.com>
2017-02-02 10:31:36 -0500

    add travis scancode support

---
+++ tools/travis/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Build script for Travis-CI.
+
+SCRIPTDIR=$(cd $(dirname "$0") && pwd)
+ROOTDIR="$SCRIPTDIR/../.."
+WHISKDIR="$ROOTDIR/../openwhisk"
+
+# Install OpenWhisk
+
+cd $WHISKDIR
+tools/build/scanCode.py $ROOTDIR
