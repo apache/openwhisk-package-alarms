e8283e1370c60a0ef5c1855a04c0c104ed56a193
Jason Peterson <jasonpet@us.ibm.com>
2017-06-05 16:11:13 -0400

    Add Redis support for managing active state

--- provider/lib/active.js
+++ provider/lib/active.js
@@ -3,30 +3,35 @@ module.exports = function(logger, providerUtils) {
   // Active Endpoint
   this.endPoint = '/active';
 
-  this.active = function (req, res) {
+  this.active = function(req, res) {
       var method = 'active';
       var response = {};
+      var active = providerUtils.active;
 
       if (req.query && req.query.active) {
-          var errorMessage = "Invalid query string";
-          try {
-              var active = JSON.parse(req.query.active);
-              if (typeof active !== 'boolean') {
-                  response.error = errorMessage;
+          var query = req.query.active.toLowerCase();
+          if (query !== 'true' && query !== 'false') {
+              response.error = "Invalid query string";
+          }
+          else if (providerUtils.active !== query) {
+              if (providerUtils.redisClient) {
+                  active = 'swapping';
+                  providerUtils.redisClient.publish(providerUtils.redisHash, "active swap");
+                  var msg = 'Running swap to change active state';
+                  logger.info(method, msg);
+                  response.message = msg;
               }
-              else if (providerUtils.active !== active) {
+              else {
+                  providerUtils.active = active = query;
                   var message = 'The active state has been changed';
                   logger.info(method, message, 'to', active);
-                  providerUtils.active = active;
                   response.message = message;
               }
           }
-          catch (e) {
-              response.error = errorMessage;
-          }
       }
-      response.active = providerUtils.active;
+      response.active = active;
       response.worker = providerUtils.worker;
+      response.host = providerUtils.host;
       res.send(response);
   };
 
