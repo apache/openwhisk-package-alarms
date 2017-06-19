4cf38e85c9b819788e596058fe54434fd1c6232b
Jason Peterson <jasonpet@us.ibm.com>
2017-06-19 13:08:48 -0400

    get all system stats in parallel

--- provider/lib/health.js
+++ provider/lib/health.js
@@ -2,37 +2,33 @@ var si = require('systeminformation');
 
 module.exports = function(utils) {
 
-  // Health Endpoint
-  this.endPoint = '/health';
-  var stats = {triggerCount: Object.keys(utils.triggers).length};
+    // Health Endpoint
+    this.endPoint = '/health';
+    var stats = {triggerCount: Object.keys(utils.triggers).length};
 
-  // Health Logic
-  this.health = function (req, res) {
-      si.mem()
-      .then(data => {
-          stats.memory = data;
-          return si.currentLoad();
-      })
-      .then(data => {
-          stats.cpu = data;
-          return si.fsSize();
-      })
-      .then(data => {
-          stats.disk = data;
-          return si.networkStats();
-      })
-      .then(data => {
-          stats.network = data;
-          return si.inetLatency(utils.routerHost);
-      })
-      .then(data => {
-          stats.apiHostLatency = data;
-          res.send(stats);
-      })
-      .catch(error =>{
-          stats.error = error;
-          res.send(stats);
-      });
-  };
+    // Health Logic
+    this.health = function (req, res) {
+
+        // get all system stats in parallel
+        Promise.all([
+            si.mem(),
+            si.currentLoad(),
+            si.fsSize(),
+            si.networkStats(),
+            si.inetLatency(utils.routerHost)
+        ])
+        .then(results => {
+            stats.memory = results[0];
+            stats.cpu = results[1];
+            stats.disk = results[2];
+            stats.network = results[3];
+            stats.apiHostLatency = results[4];
+            res.send(stats);
+        })
+        .catch(error => {
+            stats.error = error;
+            res.send(stats);
+        });
+    };
 
 };
