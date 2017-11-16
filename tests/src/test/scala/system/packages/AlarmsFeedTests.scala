4f41b89da323a0df017b505e12057b4866339a76
Adnan Baruni <abaruni@users.noreply.github.com>
2017-11-16 11:25:15 -0600

    move positive trigger get test to health class (#111)

--- tests/src/test/scala/system/packages/AlarmsFeedTests.scala
+++ tests/src/test/scala/system/packages/AlarmsFeedTests.scala
@@ -17,7 +17,7 @@
 package system.packages
 
 import org.junit.runner.RunWith
-import org.scalatest.{FlatSpec, Inside}
+import org.scalatest.FlatSpec
 import org.scalatest.junit.JUnitRunner
 import common.TestHelpers
 import common.Wsk
@@ -25,8 +25,7 @@ import common.WskProps
 import common.WskTestHelpers
 import spray.json.DefaultJsonProtocol.IntJsonFormat
 import spray.json.DefaultJsonProtocol.{LongJsonFormat, StringJsonFormat}
-import spray.json.DefaultJsonProtocol.BooleanJsonFormat
-import spray.json.{JsObject, JsString, pimpAny}
+import spray.json.pimpAny
 
 /**
  * Tests for alarms trigger service
@@ -34,7 +33,6 @@ import spray.json.{JsObject, JsString, pimpAny}
 @RunWith(classOf[JUnitRunner])
 class AlarmsFeedTests
     extends FlatSpec
-    with Inside
     with TestHelpers
     with WskTestHelpers {
 
@@ -73,69 +71,6 @@ class AlarmsFeedTests
         val activations = wsk.activation.pollFor(N = 4, Some(triggerName)).length
         println(s"Found activation size: $activations")
         activations should be(3)
-    }
-
-    it should "return correct status and configuration" in withAssetCleaner(wskprops) {
-        val currentTime = s"${System.currentTimeMillis}"
-
-        (wp, assetHelper) =>
-            implicit val wskProps = wp
-            val triggerName = s"dummyAlarmsTrigger-${System.currentTimeMillis}"
-            val packageName = "dummyAlarmsPackage"
-
-            // the package alarms should be there
-            val packageGetResult = wsk.pkg.get("/whisk.system/alarms")
-            println("fetched package alarms")
-            packageGetResult.stdout should include("ok")
-
-            // create package binding
-            assetHelper.withCleaner(wsk.pkg, packageName) {
-                (pkg, name) => pkg.bind("/whisk.system/alarms", name)
-            }
-
-            val triggerPayload = JsObject(
-                "test" -> JsString("alarmsTest")
-            )
-            val cronString = "* * * * * *"
-            val maxTriggers = -1
-
-            // create whisk stuff
-            val feedCreationResult = assetHelper.withCleaner(wsk.trigger, triggerName) {
-                (trigger, name) =>
-                    trigger.create(name, feed = Some(s"$packageName/alarm"), parameters = Map(
-                        "trigger_payload" -> triggerPayload,
-                        "cron" -> cronString.toJson))
-            }
-            feedCreationResult.stdout should include("ok")
-
-            val actionName = s"$packageName/alarm"
-            val run = wsk.action.invoke(actionName, parameters = Map(
-                "triggerName" -> triggerName.toJson,
-                "lifecycleEvent" -> "READ".toJson,
-                "authKey" -> wskProps.authKey.toJson
-            ))
-
-            withActivation(wsk.activation, run) {
-                activation =>
-                    activation.response.success shouldBe true
-
-                    inside (activation.response.result) {
-                        case Some(result) =>
-                            val config = result.getFields("config").head.asInstanceOf[JsObject].fields
-                            val status = result.getFields("status").head.asInstanceOf[JsObject].fields
-
-                            config should contain("name" -> triggerName.toJson)
-                            config should contain("cron" -> cronString.toJson)
-                            config should contain("payload" -> triggerPayload)
-                            config should contain key "namespace"
-
-                            status should contain("active" -> true.toJson)
-                            status should contain key "dateChanged"
-                            status should contain key "dateChangedISO"
-                            status should not(contain key "reason")
-                    }
-            }
-
     }
 
     it should "return error message when alarm action does not include cron parameter" in withAssetCleaner(wskprops) {
