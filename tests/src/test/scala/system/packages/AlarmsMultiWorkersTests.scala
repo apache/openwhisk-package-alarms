1f30730e4eb0851e907544b757b0d7a507ba733d
Jason Peterson <jasonpet@us.ibm.com>
2017-11-19 17:19:38 -0500

    update tests that use put on creation to now use post

--- tests/src/test/scala/system/packages/AlarmsMultiWorkersTests.scala
+++ tests/src/test/scala/system/packages/AlarmsMultiWorkersTests.scala
@@ -72,13 +72,13 @@ class AlarmsMultiWorkersTests extends FlatSpec
                 wsk.trigger.create(worker10Trigger)
 
                 //create trigger feed and assign to worker10
-                makePutCallWithExpectedResult(worker10Params, 200)
+                makePostCallWithExpectedResult(worker10Params, 200)
 
                 wsk.trigger.create(worker11Trigger)
 
                 //create trigger feed and assign to worker10 or worker11
                 //the one with the least assigned triggers will be chosen
-                makePutCallWithExpectedResult(worker11Params, 200)
+                makePostCallWithExpectedResult(worker11Params, 200)
 
                 val dbName = s"${dbPrefix}alarmservice"
                 val documents = getAllDocs(dbName)
@@ -99,12 +99,12 @@ class AlarmsMultiWorkersTests extends FlatSpec
             }
     }
 
-    def makePutCallWithExpectedResult(params: JsObject, expectedCode: Int) = {
+    def makePostCallWithExpectedResult(params: JsObject, expectedCode: Int) = {
         val response = RestAssured.given()
                 .contentType(ContentType.JSON)
                 .config(RestAssured.config().sslConfig(new SSLConfig().relaxedHTTPSValidation()))
                 .body(params.toString())
-                .put(webActionURL)
+                .post(webActionURL)
         assert(response.statusCode() == expectedCode)
     }
 
