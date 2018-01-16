03c324bb01cce7d5d32f9fd0551381864873e1fe
Jason Peterson <jasonpet@us.ibm.com>
2018-01-16 11:39:14 -0500

    support SSL for redis (#122)

--- provider/app.js
+++ provider/app.js
@@ -3,6 +3,7 @@
  * Service which can be configured to listen for triggers from a provider.
  * The Provider will store, invoke, and POST whisk events appropriately.
  */
+var URL = require('url').URL;
 var http = require('http');
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -129,9 +130,20 @@ function createRedisClient() {
 
     return new Promise(function(resolve, reject) {
         if (redisUrl) {
+            var client;
             var redis = require('redis');
             bluebird.promisifyAll(redis.RedisClient.prototype);
-            var client = redis.createClient(redisUrl);
+            if (redisUrl.startsWith('rediss://')) {
+                // If this is a rediss: connection, we have some other steps.
+                client = redis.createClient(redisUrl, {
+                    tls: { servername: new URL(redisUrl).hostname }
+                });
+                // This will, with node-redis 2.8, emit an error:
+                // "node_redis: WARNING: You passed "rediss" as protocol instead of the "redis" protocol!"
+                // This is a bogus message and should be fixed in a later release of the package.
+            } else {
+                client = redis.createClient(redisUrl);
+            }
 
             client.on('connect', function () {
                 resolve(client);
