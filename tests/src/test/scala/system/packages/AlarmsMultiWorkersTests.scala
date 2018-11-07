4706c66ed5a1638c38f1b7b05bdd415fd1ae9583
Vincent Hou <shou@us.ibm.com>
2018-11-07 14:09:00 -0500

    Rename the imported package to org.apache.openwhisk.

--- tests/src/test/scala/system/packages/AlarmsMultiWorkersTests.scala
+++ tests/src/test/scala/system/packages/AlarmsMultiWorkersTests.scala
@@ -26,9 +26,9 @@ import org.scalatest.junit.JUnitRunner
 import org.scalatest.{FlatSpec, Matchers}
 import spray.json.DefaultJsonProtocol._
 import spray.json._
-import whisk.core.WhiskConfig
-import whisk.core.database.test.ExtendedCouchDbRestClient
-import whisk.utils.{JsHelpers, retry}
+import org.apache.openwhisk.core.WhiskConfig
+import org.apache.openwhisk.core.database.test.ExtendedCouchDbRestClient
+import org.apache.openwhisk.utils.{JsHelpers, retry}
 
 import scala.concurrent.Await
 import scala.concurrent.duration.DurationInt
